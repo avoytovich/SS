40e10adbe3aa7cc714d31a7538ec8d932678b2b2
Iryna Kashchuk <iryna.kashchuk@irco.com>
2021-09-20 15:39:42 +0300

    SS-32 Add table swith styles

--- src/pages/NeighborsList.js
+++ src/pages/NeighborsList.js
@@ -3,6 +3,13 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import { TagCloud } from 'react-tagcloud';
 
 const data = [
@@ -20,6 +27,20 @@ const data = [
   { value: 'HTML5', count: 33, color: '#000000' },
   { value: 'MongoDB', count: 18, color: '#000000' },
   { value: 'CSS3', count: 10, color: '#000000' },
+];
+
+function createData(proximity, id, skillsGroup, skillName, engineersNumber) {
+  return {
+    proximity, id, skillsGroup, skillName, engineersNumber,
+  };
+}
+
+const rows = [
+  createData(0.2, 1, 'Programming Languages', '.NET', 100),
+  createData(0.18, 2, 'Java', 'Eclipse', 34),
+  createData(0.67, 3, 'Platforms', 'Maven', 2321),
+  createData(0.12, 4, 'Platforms', 'Swing', 432),
+  createData(0.01, 5, 'Java', 'JDBS', 344),
 ];
 
 const SimpleCloud = (
@@ -46,6 +67,7 @@ export default function NeighborsList() {
                     border: 1,
                     padding: 2,
                     maxWidth: { xs: 300, md: 250 },
+                    maxHeight: 125,
                   }
               } textAlign="left">
                   <Typography>Group: Programming Languages</Typography>
@@ -55,6 +77,39 @@ export default function NeighborsList() {
               <Box>
                 {SimpleCloud}
               </Box>
+          </Box>
+          <Box>
+              <div style={{ height: 400, width: '100%' }}>
+                  <TableContainer component={Paper}>
+                      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                          <TableHead>
+                              <TableRow>
+                                  <TableCell>Proximity</TableCell>
+                                  <TableCell>ID</TableCell>
+                                  <TableCell>Skills Group</TableCell>
+                                  <TableCell>Skill Name</TableCell>
+                                  <TableCell># of Engineers</TableCell>
+                              </TableRow>
+                          </TableHead>
+                          <TableBody>
+                              {rows.map(row => (
+                                  <TableRow
+                                      key={row.id}
+                                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                  >
+                                      <TableCell component="th" scope="row">
+                                          {row.proximity}
+                                      </TableCell>
+                                      <TableCell>{row.id}</TableCell>
+                                      <TableCell>{row.skillsGroup}</TableCell>
+                                      <TableCell>{row.skillName}</TableCell>
+                                      <TableCell>{row.engineersNumber}</TableCell>
+                                  </TableRow>
+                              ))}
+                          </TableBody>
+                      </Table>
+                  </TableContainer>
+              </div>
           </Box>
       </Box>
   );
