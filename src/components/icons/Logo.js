609f66c77b902a1d140329027e635c921c26b95e
Yaroslav Rudyy <yrudyy@lohika.com>
2021-11-08 10:22:05 +0000

    [SS-104] UI: Add 404 Not Found Page; Updated Welcome Page

---
+++ src/components/icons/Logo.js
@@ -0,0 +1,45 @@
+import { SvgIcon } from '@mui/material';
+import React from 'react';
+
+export const Logo = props => (
+  <SvgIcon width="192" height="192" viewBox="0 0 192 192" fill="none" {...props}>
+    <g clipPath="url(#clip0_383:1382)">
+      <path
+        d="M96.9259 186.094L143.127 164.4C152.16 159.895 151.951 151.365 151.936 143.429C151.918 133.509 151.927 138.469 162.662 118.607L162.666 118.601C173.399 98.743 178.766 88.8141 178.728 68.9755C178.69 49.1349 162.483 24.3642 135.531 14.4952C108.579 4.62623 76.2589 4.6878 54.7407 19.6093C33.2225 34.5307 33.2508 49.4111 27.8926 64.3018C22.5343 79.1925 27.9209 79.1822 27.9304 84.1424C27.9398 89.1025 11.8082 104.014 11.8177 108.974C11.8271 113.934 17.2232 118.884 22.6098 118.874C27.9965 118.863 24.9257 122.685 23.7873 131.02C23.3718 134.062 23.8086 142.222 23.8193 147.823C23.8328 154.908 28.3096 163.613 35.7788 164.604C44.0533 165.702 57.3413 168.409 65.7974 168.393C72.7803 168.38 85.971 191.716 96.9259 186.094Z"
+        fill="#0070AD"
+      />
+      <path d="M88.7344 40.5469L99.75 84.6094" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M55.6875 62.5781L99.75 84.6094" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M99.75 84.6094L127.289 57.0703" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M138.305 24.0234L127.289 57.0703" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M72.2109 35.0391L88.7344 40.5469" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M105.258 24.0234L88.7344 40.5469" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M160.336 46.0547L127.289 57.0703" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M99.75 84.6094H154.828" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M138.305 101.133L154.828 84.6094" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M149.32 112.148L154.828 84.6094" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M50.1797 35.0391L55.6875 62.5781" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M39.1641 79.1016L55.6875 62.5781" stroke="#12ABDB" strokeWidth="11.0156" />
+      <path d="M160.336 68.0859L154.828 84.6094" stroke="#12ABDB" strokeWidth="11.0156" />
+      <rect x="77.7188" y="62.5781" width="44.0625" height="44.0625" rx="22.0312" fill="white" />
+      <rect x="77.7188" y="29.5312" width="22.0312" height="22.0312" rx="11.0156" fill="white" />
+      <rect x="110.766" y="29.5312" width="33.0469" height="33.0469" rx="16.5234" fill="white" />
+      <rect x="132.797" y="18.5156" width="11.0156" height="11.0156" rx="5.50781" fill="white" />
+      <rect x="154.828" y="40.5469" width="11.0156" height="11.0156" rx="5.50781" fill="white" />
+      <rect x="132.797" y="95.625" width="11.0156" height="11.0156" rx="5.50781" fill="white" />
+      <rect x="143.812" y="106.641" width="11.0156" height="11.0156" rx="5.50781" fill="white" />
+      <rect x="44.6719" y="29.5312" width="11.0156" height="11.0156" rx="5.50781" fill="white" />
+      <rect x="33.6562" y="73.5938" width="11.0156" height="11.0156" rx="5.50781" fill="white" />
+      <rect x="66.7031" y="29.5312" width="11.0156" height="11.0156" rx="5.50781" fill="white" />
+      <rect x="99.75" y="18.5156" width="11.0156" height="11.0156" rx="5.50781" fill="white" />
+      <rect x="154.828" y="62.5781" width="11.0156" height="11.0156" rx="5.50781" fill="white" />
+      <rect x="143.812" y="73.5938" width="22.0312" height="22.0312" rx="11.0156" fill="white" />
+      <rect x="44.6719" y="51.5625" width="22.0312" height="22.0312" rx="11.0156" fill="white" />
+    </g>
+    <defs>
+      <clipPath id="clip0_383:1382">
+        <rect width="176.25" height="176.25" fill="white" transform="translate(11.625 7.5)" />
+      </clipPath>
+    </defs>
+  </SvgIcon>
+);
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,14 +2,16 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import PageTitle from '../components/PageTitle';
+import { Logo } from '../components/icons';
 
 export default function Welcome() {
   return (
     <>
       <PageTitle title="Smart Skills" includeAppName={false} />
       <Box sx={{ my: 4, flex: 1, textAlign: 'center' }}>
+        <Logo sx={{ fontSize: 120 }} />
         <Typography variant="h4" component="h1" gutterBottom>
-          Smart Skills Web App
+          Smart Skills
         </Typography>
       </Box>
     </>
