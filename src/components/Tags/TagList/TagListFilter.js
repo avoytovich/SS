d10d1fb2ad3e0bf7c4c1293737e6ac96052f91e7
Anna Vagner <ganna.vagner@changehealthcare.com>
2022-05-19 12:45:23 +0300

    tag-list: Update tag filter

--- src/components/Tags/TagList/TagListFilter.js
+++ src/components/Tags/TagList/TagListFilter.js
@@ -1,39 +1,37 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
-import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
-import {Button, Box} from '@mui/material';
+import {Box, IconButton, InputAdornment} from '@mui/material';
 import {useStyles} from 'components/Tags/TagList/styles';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 
 export function TagListFilter({tagName, onChangeTagName, onClearFilter}) {
   const classes = useStyles();
 
+  const DeleteTagButton = (
+    <IconButton aria-label="delete" size="small" onClick={onClearFilter}>
+      <CloseOutlinedIcon fontSize="small" />
+    </IconButton>
+  );
+
   const handleTagSearch = e => onChangeTagName(e.target.value);
 
   return (
-    <Box className={classes.filterContainer} data-testid="tag-list-filter">
-      <FormControl variant="standard" className={classes.form}>
-        <TextField
-          id="tag-name-input"
-          data-testid="tag-list-filter-search"
-          label="Tag Name"
-          variant="standard"
-          placeholder="Search"
-          onChange={handleTagSearch}
-          value={tagName}
-        />
-        <Button
-          variant="text"
-          data-testid="tag-list-filter-cleanup-btn"
-          size="small"
-          disabled={!tagName}
-          className={classes.cleanupButton}
-          onClick={onClearFilter}
-        >
-          Clean Up
-        </Button>
-      </FormControl>
+    <Box component="form" className={classes.filterContainer} data-testid="tag-list-filter">
+      <TextField
+        id="tag-name-input"
+        data-testid="tag-list-filter-search"
+        label="Tag Name"
+        size="small"
+        placeholder="Search"
+        className={classes.searchField}
+        onChange={handleTagSearch}
+        value={tagName}
+        InputProps={{
+          endAdornment: <InputAdornment position="end">{tagName && DeleteTagButton}</InputAdornment>
+        }}
+      />
     </Box>
   );
 }
