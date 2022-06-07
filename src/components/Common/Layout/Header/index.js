0278255a2e6179e9c53f33cb9cb85a1af8943d62
Anna Vagner <ganna.vagner@changehealthcare.com>
2022-06-07 16:31:47 +0300

    employees: Add navigationLinks

--- src/components/Common/Layout/Header/index.js
+++ src/components/Common/Layout/Header/index.js
@@ -4,12 +4,12 @@ import {NavLink as RouterLink} from 'react-router-dom';
 import {AppBar, Box, Toolbar, Link} from '@mui/material';
 
 import usePermissions from 'hooks/permissions';
-import {PermissionEnum} from 'constants/permissions';
 import routes from 'constants/routes';
 import {HeaderLogo} from 'components/icons';
 
 import UserMenu from 'components/Common/Layout/Header/UserMenu';
 import {useStyles} from 'components/Common/Layout/Header/styles';
+import {navigationLinks} from 'constants/header';
 
 const isActiveFn = path => (match, location) => location.pathname.startsWith(path);
 
@@ -17,48 +17,27 @@ const Header = () => {
   const classes = useStyles();
   const {hasPermissions} = usePermissions();
 
-  // TODO ADD filter navigation list
-
   return (
     <AppBar position="static">
       <Toolbar>
         <Link component={RouterLink} to={routes.home} exact={true} activeClassName={classes.active}>
           <HeaderLogo sx={{transform: 'scale(3.7)', marginLeft: '20px'}} />
         </Link>
-
         <Box className={classes.navContent}>
-          {hasPermissions([PermissionEnum.SKILLS_LIST]) && (
-            <Link
-              component={RouterLink}
-              to={routes.skills.list}
-              exact={true}
-              isActive={isActiveFn(routes.skills.list)}
-            >
-              Skills
-            </Link>
-          )}
-          {hasPermissions([PermissionEnum.TAGS_LIST]) && (
-            <Link
-              component={RouterLink}
-              to={routes.tags.list}
-              exact={true}
-              isActive={isActiveFn(routes.tags.list)}
-            >
-              Tags
-            </Link>
-          )}
-          {hasPermissions([PermissionEnum.USERS_LIST]) && (
-            <Link
-              component={RouterLink}
-              to={routes.employees.list}
-              exact={true}
-              isActive={isActiveFn(routes.employees.list)}
-            >
-              Employees
-            </Link>
+          {navigationLinks.map(link =>
+            hasPermissions(link.permissions) ? (
+              <Link
+                key={link.key}
+                component={RouterLink}
+                to={link.pathName}
+                exact={link.exact}
+                isActive={isActiveFn(link.pathName)}
+              >
+                {link.linkName}
+              </Link>
+            ) : null
           )}
         </Box>
-
         <UserMenu />
       </Toolbar>
     </AppBar>
